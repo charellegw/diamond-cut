530dc4f347c84600f759649ca3c92a0f0ef448b0
naomiwinarto <charys.naomi.winarto@gmail.com>
2024-06-16 04:23:57 +0700

    up

---
+++ js/cars.js
@@ -0,0 +1,17 @@
+const filterBtn = document.querySelectorAll('.filter button');
+const galleryCard = document.querySelectorAll('.carGallery .card');
+
+const filterCards = e => {
+    document.querySelector(".checked").classList.remove("active");
+    e.target.classList.add("checked");
+
+    galleryCard.forEach(card => {
+        card.classList.add("hide")
+    })
+    
+    if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all" ){
+        card.classList.remove("hide")
+    }
+}
+
+filterBtn.forEach(button => button.addEventListener("click", filterCards));
